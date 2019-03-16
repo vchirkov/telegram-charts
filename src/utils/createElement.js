988dd3a3c042ddf58ca8977e7563aedfe99e8f77
vlad.chirkov <vlad.chirkov@cloudcall.com>
2019-03-16 10:52:34 +0300

    styles for create element

--- src/utils/createElement.js
+++ src/utils/createElement.js
@@ -1,14 +1,14 @@
 const SVG_NS = "http://www.w3.org/2000/svg";
 
-module.exports.createSvgElement = function createSvgElement(tag = 'g', className, attrs) {
-    return fillWithAttrs(document.createElementNS(SVG_NS, tag), className, attrs);
+module.exports.createSvgElement = function createSvgElement(tag = 'g', className, attrs, style) {
+    return fillWithAttrs(document.createElementNS(SVG_NS, tag), className, attrs, style);
 };
 
-module.exports.createHTMLElement = function createHTMLElement(tag = 'div', className, attrs) {
+module.exports.createHTMLElement = function createHTMLElement(tag = 'div', className, attrs, style) {
     return fillWithAttrs(document.createElement(tag), className, attrs);
 };
 
-function fillWithAttrs(el, className = '', attrs = {}) {
+function fillWithAttrs(el, className = '', attrs = {}, style = {}) {
     if (className) {
         el.setAttribute('class', className);
     }
@@ -16,6 +16,8 @@ function fillWithAttrs(el, className = '', attrs = {}) {
     for (const key in attrs) {
         el.setAttribute(key, attrs[key]);
     }
+
+    Object.assign(el.style, style);
 
     return el;
 }
